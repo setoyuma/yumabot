45ea71772b4b9e242721f7e2bac304bd31afe641
setoyuma <setoyumaden@gmail.com>
2022-11-17 16:09:09 -0500

    added more tweaks, runyuma() and upgraded yuma_help_embed()

---
+++ test_yuma_unittest.py
@@ -0,0 +1,9 @@
+import unittest
+from yuma import *
+
+class TestYuma(unittest.TestCase):
+    def test_runyuma(self):
+        self.assertEqual(runyuma(ADMINKI),True,'\n\tyumabot startup failed...\n')
+        
+if __name__ == '__main__':
+    unittest.main()
--- yuma.py
+++ yuma.py
@@ -1,4 +1,9 @@
+# yuma.py
+#written by seto yuma#
 import os
+import datetime
+import getpass
+import time as t
 import discord as d
 from discord.ext import commands
 import random
@@ -6,40 +11,117 @@ import random
 from dotenv import load_dotenv
 load_dotenv()
 
-#intent setup
+'''intent setup'''
 intents = d.Intents.default()
 intents.message_content = True
 intents.messages = True
 intents.members = True
 # intents.all = True
 
-TOKEN = os.getenv('DISCORD_TOKEN')
-yumabot = commands.Bot(command_prefix='r/', intents=intents)
-
+'''bot vars'''
+TOKEN=os.getenv('DISCORD_TOKEN')
+ADMINK=os.getenv('ADMINK')
+ADMINKGET = getpass.getpass(prompt='\n\tenter admin key:')
+yumabot=commands.Bot(command_prefix='r/', intents=intents)
+with open('skull.txt','r') as skullF:
+    SKULLYBOI = skullF.read()
+    
+'''yumabot events'''
 @yumabot.event
 async def on_ready():
+    print('\n\n\t\tyumabot ascii art\n\n')
+    t.sleep(2)
     print(f'\n\t{yumabot.user.name} is online...\n')
 
+'''yumabot commands'''
+
 @yumabot.command(name='tweak',help='yumabot says something out of the box')
-async def yuma_tweak(ctx):
+async def yuma_tweak(ctx)->str:
     scuffed_responses = [
         'AHHHHHHHHHHHH AHHHHH AHHHHHHHHAHAHAHHHH',
         'bitch',
         'you think they want you drinkin coconut water?',
         'so where do gummy bears come from???',
         'if i cant smell it, must taste neutral',
-        'someone hit the lights'
+        'someone hit the lights',
+        '(/-_-)/',
+        'we tweakin???',
+        'HOLY FUCK ITS ROBERT',
+        "i'd fry you in bell ball.",
+        'ok.',
+        f'{SKULLYBOI}'
     ]
 
     response = random.choice(scuffed_responses)
     await ctx.send(response)
+    return response
 
 @yumabot.command(name='d20',help='rolls a 20 sided die')
-async def yuma_d20(ctx):
+async def yuma_d20(ctx)->int:
     sides = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
     
     player_roll = random.choice(sides)
     await ctx.send(f'\t{ctx.message.author.name} Rolled {str(player_roll)}\n')
+    return player_roll
+
+'''yumabot/user_interactive embeds'''
+@yumabot.command(name='yumahelp',help='a more comprehensive explanation of yumabot and its commands')
+async def yuma_help_embed(ctx):
+    embed=d.Embed(
+        title='***YUMAHELP***',
+        type='rich',
+        url='https://www.github.com/setoyuma',
+        description='learning embeds',
+        color=d.Color.blurple()
+        )
+    embed.set_author(name=f'{yumabot.user.name}',url='https://github.com/setoyuma',icon_url='https://avatars.githubusercontent.com/u/118138305?v=4')
+    
+    #thumbnail
+    embed.set_thumbnail(url='https://avatars.githubusercontent.com/u/118138305?v=4')
+
+    #fields
+    embed.add_field(
+        name='***commands***',
+        value='*---*',
+        inline=False
+    )
+
+    embed.add_field(
+        name='format:\n<command>',
+        value='<description of command>',
+        inline=False
+        )
+    
+    embed_line_break = embed.add_field(name='_',value='_',inline=False)
+
+    embed.add_field(
+        name='r/d20',
+        value='rolls a 20 sided die',
+        inline=True
+        )
+
+    embed.add_field(
+        name='r/tweak',
+        value='yumabot says something (generally)out of pocket.',
+        inline=True
+        )
+
+
+
+    await ctx.send(embed=embed)
+
+
+'''yumabot config functions'''
+def runyuma(spawn,admink:str)->bool:
+    if admink.capitalize() != ADMINK:
+        print('\n\tyumabot startup failed...\n\n\t\tinvalid key:',ADMINKGET)
+        t.sleep(3)
+        #log admin failures in adminlogin_fail.txt
+        os.system('clear')
+        return False
+    if admink.capitalize() == ADMINK:
+        spawn.run(TOKEN)
+        return True
 
 if __name__ == '__main__':
-    yumabot.run(TOKEN)
+    runyuma(yumabot,ADMINKGET)
